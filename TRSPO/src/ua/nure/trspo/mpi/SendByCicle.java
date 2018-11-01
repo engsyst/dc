f03e5e9cbe1659343a7fcd1421dfa8455f0564fa
engsyst <engsyst@Gig>
2018-11-01 13:06:14 +0200

    SendByCicle example added

---
+++ TRSPO/src/ua/nure/trspo/mpi/SendByCicle.java
@@ -0,0 +1,23 @@
+package ua.nure.trspo.mpi;
+
+import mpi.MPI;
+
+public class SendByCicle {
+	public static final int ROOT = 0;
+
+	public static void main(String[] args) {
+		MPI.Init(args);
+		int size = MPI.COMM_WORLD.Size();
+		int rank = MPI.COMM_WORLD.Rank();
+
+		int left = rank == 0 ? size - 1 : rank - 1; 
+		int right = rank == size - 1 ? 0 : rank + 1;
+		int[] sendbuf = new int[] {rank};
+		int[] recvbuf = new int[1];
+		MPI.COMM_WORLD.Sendrecv(sendbuf, 0, 1, MPI.INT, right, 1, recvbuf, 0, 1, MPI.INT, left, 1);
+		System.out.println("Process " + rank + ": Send -> " + sendbuf[0] + ", receive " + recvbuf[0]);
+		
+		MPI.Finalize();
+
+	}
+}
